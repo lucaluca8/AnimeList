025cf6bb48a02ae782eb3ad4f1846fb32ec60070
Lucal <lucaluca_9@yahoo.com>
2022-04-04 17:40:51 +0300

    tehnical problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HomeModModule } from './home-mod/home-mod.module';
 
 @NgModule({
   declarations: [
@@ -18,9 +15,9 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    HomeModModule,
     RouterModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule
   ],
   providers: [],
